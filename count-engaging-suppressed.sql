9315f9522d4fbb1f3b1279265daf1f40eed0c567
Osvaldo Gago <opgago@yahoo.com>
2018-05-19 17:01:15 +0200

    Count EN suppressed

---
+++ count-engaging-suppressed.sql
@@ -0,0 +1,20 @@
+/*
+
+COUNT SUPPRESSED
+
+*/
+
+
+-- TOTALS
+
+SELECT Suppressed, COUNT(Suppressed) AS Total
+FROM en_users
+GROUP BY Suppressed;
+
+-- SUPPRESSED BY TIPO
+
+SELECT Suppressed, tipo, COUNT(Suppressed) AS Total
+FROM en_users
+WHERE tipo IN (1, 2, 3, 4, 5)
+GROUP BY Suppressed, tipo;
+
