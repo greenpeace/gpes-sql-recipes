117e2db8f79d6453a58f4f7367eb48e1f6ef983e
Osvaldo Gago <opgago@yahoo.com>
2018-05-21 01:13:58 +0200

    Relevant date is  21

--- count-engaging-user-dates.sql
+++ count-engaging-user-dates.sql
@@ -15,7 +15,7 @@ SET Date_Modified = STR_TO_DATE(Date_Modified, '%d/%m/%Y');
 -- COUNT USERS BETWEEN 2 DATES
 
 SELECT COUNT(*) FROM en_users
-WHERE Date_Created BETWEEN '2014-03-22' AND '2014-12-31';
+WHERE Date_Created BETWEEN '2014-03-21' AND '2014-12-31';
 
 SELECT COUNT(*) FROM en_users
 WHERE Date_Modified BETWEEN '2018-05-19' AND '2018-05-19';
@@ -28,7 +28,7 @@ WHERE Date_Modified = '0000-00-00';
 -- COMPLEX STUFF
 
 SELECT COUNT(*) FROM en_users
-WHERE (Date_Created BETWEEN '2014-03-22' AND '2014-12-31')
+WHERE (Date_Created BETWEEN '2014-03-21' AND '2014-12-31')
 AND
 ((Date_Modified BETWEEN '2018-05-01' AND '2018-05-19') OR Date_Modified = '0000-00-00');
 
