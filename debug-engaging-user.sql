46986e7c16274d37425cbf7f8a327f0bf42b67f3
Osvaldo Gago <opgago@yahoo.com>
2018-05-20 19:39:28 +0200

    Debug Engaging User

---
+++ debug-engaging-user.sql
@@ -0,0 +1,44 @@
+/*
+
+DEBUG PROBLEMS WTH ENGAGING USER
+
+*/
+
+-- UPDATE WHERE TIPO IS NULL AND EMPTY
+
+UPDATE en_users
+SET tipo = 1
+WHERE tipo IS NULL;
+
+UPDATE en_users
+SET tipo = 1
+WHERE tipo = '';
+
+-- MULTIPLE CONTACT ID AND LEAD ID
+
+SELECT * FROM en_users
+WHERE SFDC_Contact_ID <> '' AND SFDC_Lead_ID <> '';
+
+-- EMPTY CONTACT ID FOR A CONTACT
+
+SELECT * FROM en_users
+WHERE ( contact_codes <> '' AND contact_codes <> 'Donor Prospect')
+AND `SFDC_Contact_ID` = '';
+
+-- EMPTY LEAD ID FOR A QUALIFIED LEAD
+
+SELECT * FROM en_users
+WHERE SFDC_lead_id = '' AND
+contact_codes = '' AND
+first_name <> '' AND
+last_name <> '' AND
+id_number <> '' AND
+phone_number <> '';
+
+-- MISSING CONTACT CODES AND SFDC_contact_id
+
+SELECT * FROM en_users
+WHERE tipo IN (0,2,3,4,5) AND contact_codes = '';
+
+SELECT * FROM en_users
+WHERE tipo IN (0,2,3,4,5) AND SFDC_contact_id = '';
