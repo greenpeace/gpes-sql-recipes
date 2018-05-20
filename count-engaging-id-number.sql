6ef1635864b6601b48766e7cd70f37e0d7a4d20e
Osvaldo Gago <opgago@yahoo.com>
2018-05-20 12:23:02 +0200

    Id number

--- count-engaging-id-number.sql
+++ count-engaging-id-number.sql
@@ -14,8 +14,14 @@ SELECT COUNT(id_number) AS 'Phone numbers with id number format'
 FROM en_users
 WHERE id_number REGEXP '^[A-z]?[0123456789]{7,8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]$';
 
--- COUNT RECORDS WITH INVALID OR EMPTY ID NUMBER
+-- COUNT RECORDS WITH INVALID (wrong format) OR EMPTY ID NUMBER
 
 SELECT COUNT(id_number) AS 'Phone numbers with id number format'
 FROM en_users
 WHERE id_number REGEXP '^[A-z]?[0123456789]{7,8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]$';
+
+-- SHOW RECORDS WITH INVALID (wrong format) ID NUMBER
+
+SELECT *
+FROM en_users
+WHERE NOT id_number REGEXP '^[A-z]?[0123456789]{7,8}[TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke]$' AND id_number <> '';
