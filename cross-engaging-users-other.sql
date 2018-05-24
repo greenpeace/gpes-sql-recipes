446cd1bd09c4b60966432e68f866db74559c8351
Osvaldo Gago <opgago@yahoo.com>
2018-05-24 20:39:34 +0200

    Select to export

--- cross-engaging-users-other.sql
+++ cross-engaging-users-other.sql
@@ -11,3 +11,10 @@ UPDATE en_users
 JOIN opt_outs
 ON en_users.Supporter_ID = opt_outs.Supporter_ID
 SET en_users.real_email_ok = opt_outs.real_email_ok;
+
+-- MAKE A SELECT AND EXPORT IT EITHER AS CSV OR TABLE
+
+SELECT * FROM en_users
+WHERE tipo = 1
+AND
+(Suppressed = 'Y' OR real_email_ok = 'N' OR soft_bounce = 'Y'  OR privacy_email_sent IS NULL);
