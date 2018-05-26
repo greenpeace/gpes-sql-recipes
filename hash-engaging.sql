2b33fd29440416ed73a8db2eeefbad5c9b226763
Osvaldo Gago <opgago@yahoo.com>
2018-05-26 10:54:52 +0200

    Remove limit

--- hash-engaging.sql
+++ hash-engaging.sql
@@ -10,4 +10,4 @@ SELECT SHA2( email, 256) AS 'email' FROM en_users;
 
 -- HASH PHONE NUMBER
 
-SELECT SHA2( phone_number, 256) AS 'phone_number' FROM en_users LIMIT 100;
+SELECT SHA2( phone_number, 256) AS 'phone_number' FROM en_users;
