a7cab3b67debc653017c213399593b99a3a9c60f
Osvaldo Gago <opgago@yahoo.com>
2018-06-10 10:14:54 +0200

    Add view with hashed data

--- create-views.sql
+++ create-views.sql
@@ -10,3 +10,11 @@ CREATE VIEW donnors AS
 SELECT *
 FROM `en_users`
 WHERE tipo IN (0,2,3,4,5);
+
+
+-- VIEW WITH HASHED DATA
+
+CREATE VIEW hashed_contact_data AS
+SELECT first_name, last_name, SHA2( email, 256) AS hashed_email , SHA2( phone_number, 256) AS hashed_phone_number
+FROM `en_users`;
+
--- hash-engaging.sql
+++ hash-engaging.sql
@@ -11,3 +11,6 @@ SELECT SHA2( email, 256) AS 'email' FROM en_users;
 -- HASH PHONE NUMBER
 
 SELECT SHA2( phone_number, 256) AS 'phone_number' FROM en_users;
+
+-- See create-views.sql for a view with hashed data.
+
